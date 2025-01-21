16f5db2475160d1541685eafe439d0518b16ba11
Neil Lin <nlin@necc.org>
2025-01-20 23:37:26 -0500

    fixed, works

--- autograd/engine.cpp
+++ autograd/engine.cpp
@@ -2,14 +2,19 @@
 
 Value::Value(double data, std::unordered_set<std::shared_ptr<Value>> children,
              char op, double grad, const std::string &label)
-    : data(data), grad(grad), op(op), label(label), prev(children) {}
+    : data(data), grad(grad), op(op), label(label), prev(children), backward([]() -> void {}) {}
 
-std::shared_ptr<Value> Value::operator+(const std::shared_ptr<Value>& other) const {
-    std::shared_ptr<Value> out = std::make_shared<Value>(
-        this->data + other->data,
-        std::unordered_set<std::shared_ptr<Value>>{shared_from_this(), other},
-        '+');
+std::shared_ptr<Value> operator+(const std::shared_ptr<Value>& a, const std::shared_ptr<Value>& b) {
+    return a->operator+(b); 
+} 
+
+std::shared_ptr<Value> Value::operator+(const std::shared_ptr<Value>& other) {
+    std::unordered_set<std::shared_ptr<Value>> prev_out;
+    prev_out.insert(shared_from_this());
+    prev_out.insert(other);
     
+    std::shared_ptr<Value> out = std::make_shared<Value>(this->data + other->data, prev_out, '+');
+
     out->backward = [self=shared_from_this(), other, out]() {
         self->grad += 1.0 * out->grad;
         other->grad += 1.0 * out->grad;
@@ -17,16 +22,24 @@ std::shared_ptr<Value> Value::operator+(const std::shared_ptr<Value>& other) con
     return out;
 }
 
-std::shared_ptr<Value> Value::operator-(const std::shared_ptr<Value>& other) const {
-    return shared_from_this() + (other * make_shared<Value>(-1));
+std::shared_ptr<Value> operator-(const std::shared_ptr<Value>& a, const std::shared_ptr<Value>& b) {
+    return a->operator-(b);
 }
 
-std::shared_ptr<Value> Value::operator*(const std::shared_ptr<Value>& other) const {
-    auto out = std::make_shared<Value>(
-        this->data * other->data,
-        std::unordered_set<std::shared_ptr<Value>>{shared_from_this(), other},
-        '*');
-    
+std::shared_ptr<Value> Value::operator-(const std::shared_ptr<Value>& other) {
+    return shared_from_this() + (other * std::make_shared<Value>(-1));
+}
+
+std::shared_ptr<Value> operator*(const std::shared_ptr<Value>& a, const std::shared_ptr<Value>& b) {
+    return a->operator*(b);
+} 
+
+std::shared_ptr<Value> Value::operator*(const std::shared_ptr<Value>& other) {
+    std::unordered_set<std::shared_ptr<Value>> prev_out;
+    prev_out.insert(shared_from_this());
+    prev_out.insert(other);
+    auto out = std::make_shared<Value>(this->data * other->data, prev_out, '*');
+
     out->backward = [self=shared_from_this(), other, out]() {
         self->grad += other->data * out->grad;
         other->grad += self->data * out->grad;
@@ -34,41 +47,43 @@ std::shared_ptr<Value> Value::operator*(const std::shared_ptr<Value>& other) con
     return out;
 }
 
-std::shared_ptr<Value> Value::operator/(const std::shared_ptr<Value>& other) const {
+std::shared_ptr<Value> operator/(const std::shared_ptr<Value>& a, const std::shared_ptr<Value>& b) {
+    return a->operator/(b);
+}
+
+std::shared_ptr<Value> Value::operator/(const std::shared_ptr<Value>& other) {
     return shared_from_this() * other->pow(-1);
 }
 
-std::shared_ptr<Value> Value::pow(const double pwr) const {
-    auto out = std::make_shared<Value>(
-        std::pow(this->data, pwr),
-        std::unordered_set<std::shared_ptr<Value>>{shared_from_this()},
-        '^');
-    
+std::shared_ptr<Value> Value::pow(const double pwr) {
+    std::unordered_set<std::shared_ptr<Value>> prev_out;
+    prev_out.insert(shared_from_this());
+    auto out = std::make_shared<Value>(std::pow(this->data, pwr), prev_out, '^');
+
     out->backward = [self=shared_from_this(), pwr, out]() {
         self->grad += (pwr * std::pow(self->data, pwr - 1)) * out->grad;
     };
     return out;
 }
 
-std::shared_ptr<Value> Value::exp() const {
-    auto out = std::make_shared<Value>(
-        std::exp(this->data),
-        std::unordered_set<std::shared_ptr<Value>>{shared_from_this()},
-        'e');
-    
+std::shared_ptr<Value> Value::exp() {
+    std::unordered_set<std::shared_ptr<Value>> prev_out;
+    prev_out.insert(shared_from_this());
+    auto out = std::make_shared<Value>(std::exp(this->data), prev_out, 'e');
+
     out->backward = [self=shared_from_this(), out]() {
         self->grad += out->data * out->grad;
     };
     return out;
 }
 
-std::shared_ptr<Value> Value::tanh() const {
-    // Value out;
-    // return out;
-    return nullptr;
+std::shared_ptr<Value> Value::tanh() {
+    auto exp = (std::make_shared<Value>(2) * shared_from_this())->exp();
+    auto one = std::make_shared<Value>(1);
+    return (exp - one)/(exp + one);
 }
 
-std::shared_ptr<Value> Value::relu() const {
+std::shared_ptr<Value> Value::relu() {
     // Value out;
     // return out;
     return nullptr;
--- autograd/engine.h
+++ autograd/engine.h
@@ -8,14 +8,15 @@
 #include <vector>
 #include <algorithm>
 #include <memory>
+#include <iostream>
 
 class Value : public std::enable_shared_from_this<Value> {
 private:
-    std::function<void()> backward;
-    std::unordered_set<std::shared_ptr<Value>> prev; 
+    std::function<void()> backward; 
+    std::unordered_set<std::shared_ptr<Value>> prev;
 
-    char op; // Debug: operation type
-    std::string label; // Debug: label for the node
+    char op; 
+    std::string label; 
 
     std::vector<std::shared_ptr<Value>> internalTopoSort();
 
@@ -23,22 +24,27 @@ public:
     double data;
     double grad;
 
-    Value(double data, std::unordered_set<std::shared_ptr<Value>> children = {}, double grad = 0.0, char op = '\0', const std::string& label = "");
+    Value(double data, std::unordered_set<std::shared_ptr<Value>> children = {}, char op = '\0', double grad = 0.0, const std::string& label = "");
 
-    std::shared_ptr<Value> operator+(const std::shared_ptr<Value>& other) const;
-    std::shared_ptr<Value> operator-(const std::shared_ptr<Value>& other) const;
-    std::shared_ptr<Value> operator*(const std::shared_ptr<Value>& other) const;
-    std::shared_ptr<Value> operator/(const std::shared_ptr<Value>& other) const;
-    
-    std::shared_ptr<Value> pow(const double pwr) const;
-    std::shared_ptr<Value> exp() const;
+    std::shared_ptr<Value> operator+(const std::shared_ptr<Value>& other);
+    std::shared_ptr<Value> operator-(const std::shared_ptr<Value>& other);
+    std::shared_ptr<Value> operator*(const std::shared_ptr<Value>& other);
+    std::shared_ptr<Value> operator/(const std::shared_ptr<Value>& other);
+
+    std::shared_ptr<Value> pow(const double pwr);
+    std::shared_ptr<Value> exp();
 
     // Activation functions
-    std::shared_ptr<Value> tanh() const;
-    std::shared_ptr<Value> relu() const;
+    std::shared_ptr<Value> tanh();
+    std::shared_ptr<Value> relu();
 
     void backprop();
     friend std::ostream& operator<<(std::ostream& os, const Value& obj);
 };
+
+std::shared_ptr<Value> operator+(const std::shared_ptr<Value>& a, const std::shared_ptr<Value>& b);
+std::shared_ptr<Value> operator-(const std::shared_ptr<Value>& a, const std::shared_ptr<Value>& b);
+std::shared_ptr<Value> operator*(const std::shared_ptr<Value>& a, const std::shared_ptr<Value>& b);
+std::shared_ptr<Value> operator/(const std::shared_ptr<Value>& a, const std::shared_ptr<Value>& b);
 
 #endif
